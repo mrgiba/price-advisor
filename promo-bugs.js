5b642f2e97b78b4a12044e28aff98ebd3c8db41f
Givanildo Dantas Alves <mrgiba@users.noreply.github.com>
2015-12-20 20:37:32 -0200

    Using cheerio to retrieve text from html

--- promo-bugs.js
+++ promo-bugs.js
@@ -1,25 +1,46 @@
-var crawlerjs = require('crawler-js');
+var crawlerjs = require('crawler-js'),
+    cheerio = require('cheerio');
+// selector: 'ol.discussionListItems li.discussionListItem div.main h3.title a.PreviewTooltip',
+
 
 var worlds = {
     interval: 1000,
     getSample: 'http://www.promobugs.com.br/forums/promocoes.4/',
-    get: 'http://www.promobugs.com.br/forums/promocoes.4/',
+    get: 'http://www.promobugs.com.br/forums/promocoes.4/page-[numbers:1:1:1]',
     preview: 0,
     extractors: [
         {
-            selector: 'ol.discussionListItems li.discussionListItem div.main h3.title a.PreviewTooltip',
+            selector: 'ol.discussionListItems li.discussionListItem',
             callback: function(err, html, url, response){
                 console.log('Crawled url:');
                 console.log(url);
                 //console.log(response); // If you need see more details about request
                 if(!err){
-                    data = {};
-                    //data.world = html.children('a').eq(1).attr('href');
-                    data.url = html.attr('href');
-                    if(typeof data.url == 'undefined'){
-                        delete data.url;
+                    //data = {};
+                    ////data.world = html.children('a').eq(1).attr('href');
+                    //data.url = html.attr('href');
+                    //if(typeof data.url == 'undefined'){
+                    //    delete data.url;
+                    //}
+                    //console.log(data);
+                    //console.log(html);
+                    $ = cheerio.load(html.html());
+
+                    //console.log($('div.main').html());
+
+                    var offerId = html.attr('id');
+
+                    var previewToolTip = $('div.main h3.title a.PreviewTooltip')
+                    var offerUrl = previewToolTip.attr('href');
+                    var offerHeadline = previewToolTip.text();
+                    if(offerUrl) {
+                        console.log("Offer URL: " + offerUrl);
+                        console.log("Offer headline: " + offerHeadline);
+                        console.log("Offer ID: " + offerId);
                     }
-                    console.log(data);
+
+                    //TODO: evaluate each headline against keywords
+                    //TODO: store each new entry in the database
                 }else{
                     console.log(err);
                 }
@@ -27,5 +48,7 @@ var worlds = {
         }
     ]
 }
+
+// TODO: setup schedule to run crawler in a regular basis
 
 crawlerjs(worlds);
